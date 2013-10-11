4da45df6dc56f9f1128ec10e32f929c6e76432a5
James Wynne III <wynnejr@ornl.gov>
2013-10-11 10:49:42 -0400

    Update serialpi.c

--- serialpi.c
+++ serialpi.c
@@ -14,16 +14,16 @@ void main(int argc, char* argv[])
 	
 	srand48(((unsigned)time(&t)));				//Give rand() a seed value
 
-	for (i=0; i<=niter; ++i)					//main loop
+	for (i=0; i<=niter; ++i)				//main loop
 	{
-		x = (double)drand48();//RAND_MAX;			//gets a random x coordinate
-		y = (double)drand48();//RAND_MAX;			//gets a random y coordinate
-		z = ((x*x)+(y*y));				//Checks to see if number in inside unit square
+		x = (double)drand48();//RAND_MAX;		//gets a random x coordinate
+		y = (double)drand48();//RAND_MAX;		//gets a random y coordinate
+		z = ((x*x)+(y*y));				//Checks to see if number in inside unit circle
 		if (z<=1)
 		{
 			++count;				//if it is, consider it a valid random point	
 		}	
 	}			
-	pi = ((double)count/(double)niter)*4.0;				//p = 4(m/n)
+	pi = ((double)count/(double)niter)*4.0;			//p = 4(m/n)
 	printf("Pi: %f\n", pi);			
 }
